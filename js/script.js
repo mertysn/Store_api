f1adcccadb7ca4c37eff7c484fb30fbe27840a25
unknown <mertyasin1644@gmail.com>
2023-11-21 12:16:20 +0300

    mesaj

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@
 //! async await 
 
 const row = document.querySelector('.row')
+let sepet =[]
 
 async function fetchData() {
    try {
-    let response = await fetch('https://api.escuelajs.co/api/v1/products')
+    let response = await fetch('https://fakestoreapi.com/products')
     let data = await response.json()
     return data
    } catch (eror) {
@@ -24,7 +25,7 @@ fetchData()
         //     console.log(data[i].title)
         // }
 
-        let sepet =[]
+        
 
         data.forEach((urun) => {
             // console.log(urun.title)
@@ -46,7 +47,7 @@ fetchData()
 
             const img = document.createElement('img')
 
-            img.src = urun.category.image
+            img.src = urun.image
             img.style.width = '100%'
             img.style.height = '100%'
 
@@ -58,7 +59,7 @@ fetchData()
             baslik.textContent = urun.title
 
             const aciklama = document.createElement('p')
-            aciklama.textContent = `${urun.description} - ${urun.price}$`
+            aciklama.textContent = `${urun.price}$`
 
             const btn = document.createElement('button')
             btn.classList.add('btn', 'btn-warning')
@@ -68,6 +69,18 @@ fetchData()
                 console.log(urun)
 
                 sepet.push(urun)
+                sepet.forEach(urun => {
+
+                    const urunAdi = document.createElement('p')
+                    urunAdi.textContent = urun.title
+            
+                    cart.append(urunAdi)
+                })
+
+                let sepetJSON = JSON.stringify(sepet)
+                console.log(sepetJSON)
+
+                localStorage.setItem('sepet', sepetJSON)
 
                 console.log(sepet)
             })
@@ -91,4 +104,36 @@ fetchData()
         })
 
 
+    })
+
+    // localStorage.clear()
+
+    //! shopping kartı çekiyoruz
+    const cartIcon = document.querySelector('.fa-cart-shopping')
+    const cart = document.querySelector('#sepet')
+
+    cartIcon.addEventListener('click', () => {
+        cart.classList.toggle('aktif')
+    })
+    
+    // console.log(Boolean(sepet))
+
+    sepet.forEach(urun => {
+
+        const urunAdi = document.createElement('p')
+        urunAdi.textContent = urun.title
+
+        cart.append(urunAdi)
+    })
+
+    let localSepet = localStorage.getItem('sepet')
+    let normalSepet = JSON.parse(localSepet)
+
+    console.log(normalSepet)
+
+    normalSepet.forEach(urun => {
+        const baslik = document.createElement('p')
+        baslik.textContent = urun.title
+
+        cart.append(baslik)
     })
